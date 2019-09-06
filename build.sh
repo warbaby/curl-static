e61486963378251feeba506e34fa3f109bdad2c2
warbaby <warbaby@163.com>
2019-09-06 10:20:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 cat <<EOF | docker run -i --rm -v "$PWD":/out -w /root alpine /bin/sh
 apk add gcc make musl-dev openssl-dev
-wget https://curl.haxx.se/download/curl-7.63.0.tar.xz
-tar xvfJ curl-7.63.0.tar.xz 
+wget https://github.com/curl/curl/releases/download/curl-7_65_3/curl-7.65.3.tar.xz
+tar xvfJ curl-7.65.3.tar.xz 
 cd curl-*
 ./configure --disable-shared --with-ca-fallback
 make curl_LDFLAGS=-all-static 
